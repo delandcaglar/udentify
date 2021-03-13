cedfb741fe5eddafcf9a64a7c4470b6341a38718
deland caglar <deland-caglar@hotmail.com>
2021-03-13 04:09:14 +0000

    Delete bot_udentify_win_v1.1 directory

--- bot_udentify_win_v1.1/secrets1.py
+++
@@ -1,8 +0,0 @@
-username = 'delandcaglar@hotmail.com'
-password = 'caglar2020'
-
-username2 = 'Udentify@demo.com'
-password2 = 'Udentify1'
-
-username1 = 'ebebek1@ebebek.com'
-password1 = 'Ebebek1?'
